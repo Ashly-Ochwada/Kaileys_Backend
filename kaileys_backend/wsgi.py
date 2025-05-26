e7fbf4ec21f13a8d70fa16135b16ba0d61d0ec7f
ashlyochwada <ashly@carble.co>
2025-05-26 15:01:16 +0300

    added create_admin file

--- kaileys_backend/wsgi.py
+++ kaileys_backend/wsgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaileys_backend.settings')
 
+
+
+# Auto-create superuser if not exists
+try:
+    from kaileys_app.create_admin import run
+    run()
+except Exception as e:
+    print("Superuser creation error:", e)
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
