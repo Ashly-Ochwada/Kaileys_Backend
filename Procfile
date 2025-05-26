733f06bef6b5ff3717176552fff3df1d6d5afdd8
ashlyochwada <ashly@carble.co>
2025-05-26 13:10:56 +0300

    Prepare for Render deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kaileys_backen.wsgi
--- kaileys_backend/settings.py
+++ kaileys_backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rmj*05fkh62#wy2y-=)^z^rrhr6wuttz9!6wz-oc=cj0gvt5c3'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-rmj*05fkh62#wy2y-=)^z^rrhr6wuttz9!6wz-oc=cj0gvt5c3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-dev-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 # ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = ['.onrender.com']
@@ -98,7 +104,6 @@ WSGI_APPLICATION = 'kaileys_backend.wsgi.application'
 #     }
 # }
 
-import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
